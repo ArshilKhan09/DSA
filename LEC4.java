40cc8ef6b1faa8c34baf1cdf41f39c7db8735e9d
ArshilKhan09 <arshilkhan529@gmail.com>
2025-08-08 19:44:32 +0530

    Updated Code

--- LEC4.java
+++ LEC4.java
@@ -8,12 +8,24 @@ public class LEC4 {
         System.out.print("Enter the Number : ");
         int number = sc.nextInt();
 
-         if (number == number*1 && number == number*number ) {
-            System.out.println(number+" is Prime Number ");
-         }
-         else {
-            System.out.println(number+" is not a Prime Number ");
-         }
+         boolean isPrime = true;
+
+        if (number <= 1) {
+            isPrime = false; // 0 and 1 are not prime
+        } else {
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    isPrime = false; // divisible by some number other than 1 and itself
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(number + " is a Prime number");
+        } else {
+            System.out.println(number + " is NOT a Prime number");
+        }
         sc.close();
     }
 }
