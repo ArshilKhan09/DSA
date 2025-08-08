b03dd2b92f4bbe8ead3f1ac81b69a42a812212ee
ArshilKhan09 <arshilkhan529@gmail.com>
2025-08-08 22:04:42 +0530

    Added New File

---
+++ Pattern4.java
@@ -0,0 +1,26 @@
+public class Pattern4 {
+     /* Print the Pattern 
+
+     INVERTED HALF PYRAMID :-
+    
+     * * * * * 
+     * * * * 
+     * * *
+     * *
+     * 
+      
+     */
+
+
+     public static void main(String[] args) {
+        int n = 5 ;
+       
+
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = i ; j <= n ; j++){
+                System.out.print(" *");
+            }
+            System.out.println();    
+        }
+     }
+}
