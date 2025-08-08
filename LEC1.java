d91fc16202de4c84387360499b5b086a6f43281b
ArshilKhan09 <arshilkhan529@gmail.com>
2025-08-08 11:46:54 +0530

    Updated File

--- LEC1.java
+++ LEC1.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class LEC1 {
+    public static void main(String[] args) {
+        printNumber();
+    }      
+
+    public static void printNumber() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        int input = sc.nextInt();
+        System.out.println(input);
+        sc.close();
     
+    }
 }
+
