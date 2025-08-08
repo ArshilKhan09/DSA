9644a6af493973533b36f38f82e149a1ba330a8b
ArshilKhan09 <arshilkhan529@gmail.com>
2025-08-08 12:52:03 +0530

    Updated LEC3.java File

--- LEC3.java
+++ LEC3.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class LEC3 {
     /*Given the integer day denoting the day number,
       print on the screen which day of the week it is.
@@ -8,6 +10,44 @@ public class LEC3 {
 
     public static void main(String[] args) {
         
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Day Number : " );
+        int number = sc.nextInt();
+
+        switch (number) {
+          case 1 :
+            System.out.println("Monday");
+            break;
+
+          case 2 :
+            System.out.println("Tuesday");
+            break;
+
+          case 3 :
+            System.out.println("Wednesday");
+            break;
+
+          case 4 :
+            System.out.println("Thursday");
+            break;
+
+          case 5 :
+            System.out.println("Friday");
+            break;
+
+          case 6 :
+            System.out.println("Saturday");
+            break;
+
+          case 7 :
+            System.out.println("Sunday");
+            break;
+        
+          default:
+            System.out.println("Invalid ");
+            break;
+        }
+        sc.close();
     }
 
 }
