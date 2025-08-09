79855a29d308ca03633bc117f7c236203c79fae1
ArshilKhan09 <arshilkhan529@gmail.com>
2025-08-09 21:20:27 +0530

    Updated Pattern4.java File

--- Pattern4.java
+++ Pattern4.java
@@ -18,7 +18,7 @@ public class Pattern4 {
 
         for(int i = 1 ; i <= n ; i++){
             for(int j = i ; j <= n ; j++){
-                System.out.print(" *");
+                System.out.print("* ");
             }
             System.out.println();    
         }
