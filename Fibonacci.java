5df3fa51a4d4a6726296007f3718e14466f64979
ArshilKhan09 <arshilkhan529@gmail.com>
2025-08-18 21:22:51 +0530

    Updated File

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,3 +1,27 @@
+import java.util.Scanner;
 public class Fibonacci {
-    
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in); 
+        System.out.print("Enter the Number to Find Fibonacci Series Of : ");
+        int num = input.nextInt();
+
+        if(num<=0){
+            System.out.println("Invalid Number");
+        }
+        
+        else{
+        int start = 0;
+        int end = 1 ;
+            System.out.print(start+" ");
+            System.out.print(end+" ");
+        while (start+end<=num) {
+            int curr = start+end ;
+            start = end ;
+            end = curr ;
+            System.out.print(curr+" ");
+        }
+        }
+       
+       input.close();
+    }
 }
