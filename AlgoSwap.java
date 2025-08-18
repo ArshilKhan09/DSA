6e16e1b9ab55e6d6498a6da59437c61d38444742
ArshilKhan09 <arshilkhan529@gmail.com>
2025-08-18 17:47:40 +0530

    Updated File

--- AlgoSwap.java
+++ AlgoSwap.java
@@ -1,3 +1,15 @@
 public class AlgoSwap {
-    
+    public static void main(String[] args) {
+        int a ;
+        int b ;
+        int temp;
+
+        a=1 ;
+        b=4 ;
+        temp = a ;
+        a = b ;
+        b = temp ;
+        System.out.println(a);
+        System.out.println(b);
+    }
 }
