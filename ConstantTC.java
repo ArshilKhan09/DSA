97ce9d1a05352b0b1bd2f0ac3d363f76116097d2
ArshilKhan09 <arshilkhan529@gmail.com>
2025-08-20 17:08:59 +0530

    Updated File

--- ConstantTC.java
+++ ConstantTC.java
@@ -1,3 +1,12 @@
+import java.util.Scanner;
+
 public class ConstantTC {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Numer : ");
+        int n = input.nextInt();
+        n *= (n+1)/2;
+        System.out.println(n);
+        input.close();
+    }
 }
