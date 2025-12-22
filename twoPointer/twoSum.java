c049152aab0d9bea759df533c5dcbe0d578a5263
ArshilKhan09 <arshilkhan529@gmail.com>
2025-12-22 14:59:30 +0530

    Updated the twoSum.java

--- twoPointer/twoSum.java
+++ twoPointer/twoSum.java
@@ -1,8 +1,8 @@
 import java.util.Arrays;
 
 // Question return 2 Number from Array whose Sum is equal to Target without Creating an extra space 
-    // array is [7,2,5,1,3] 
-    // Target = 8
+// array is [7,2,5,1,3] 
+// Target = 6
 public class twoSum {
     public static void main(String[] args) {
     
@@ -13,10 +13,34 @@ public class twoSum {
         Arrays.sort(arr);
         System.out.print("Sorted Array is :");
         System.out.println(Arrays.toString(arr));
+
+        findTarget(arr,12);
     }
 
+     public static void findTarget(int[] arr, int target) {
 
-    public void FindTarget(arr){
-        
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+
+            int sum = arr[left] + arr[right];
+
+            if (sum == target) {
+                System.out.println(
+                    "The numbers whose sum is " + target + " are : "
+                    + arr[left] + ", " + arr[right]
+                );
+                return; 
+            } 
+            else if (sum < target) {
+                left++; 
+            } 
+            else {
+                right--; 
+            }
+        }
+
+        System.out.println("There are no two numbers whose sum is " + target);
     }
 }
