80cb20938c41a3e9bcf733ee63132084d6d9befd
ArshilKhan09 <arshilkhan529@gmail.com>
2025-12-22 18:38:02 +0530

    Updated twoSum.java File

--- twoPointer/twoSum.java
+++ twoPointer/twoSum.java
@@ -1,3 +1,28 @@
 public class twoSum {
-    
+    public static void main(String[] args) {
+       int[] nums = {3,2,4};
+       int target = 6 ;
+
+       Solution(nums, target);
+
+    }
+    public static void Solution(int[] nums , int target){
+        int left = 0 ;
+        int right = nums.length-1;
+
+        while (left < right) {
+            int sum = nums[left]+nums[right];
+
+            if (sum == target) {
+                return new int[] {nums[left] , nums[right]} ;
+            }
+            else if (sum < target){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        
+    }
 }
