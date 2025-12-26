2aa1280d3a0cf23a1815a099eaf125603cef295d
ArshilKhan09 <arshilkhan529@gmail.com>
2025-12-26 22:19:02 +0530

    Deleted LinkedList/basicLL.java File

--- LinkedList/basicLL.java
+++
@@ -1,8 +0,0 @@
-public class basicLL {
-    public static void main(String[] args) {
-        // We Can Insert and Delete anywhere in LinkedList not like Array
-        // Random access is not Easy like Array
-
-        
-    }
-}
