2946bd47c9f88e851297b8e3c22904f96baf0703
ArshilKhan09 <arshilkhan529@gmail.com>
2025-12-29 18:27:10 +0530

    New sqSortArr.class File has been Added

--- twoPointer/sqSortArr.java
+++ twoPointer/sqSortArr.java
@@ -1,5 +1,26 @@
 public class sqSortArr {
     public static void main(String[] args) {
-        
+        int[] arr = {-4,-1,0,3,10} ;
+
+        for(int i=0 ; i<arr.length ; i++){
+            arr[i] = arr[i]*arr[i] ;
+        }
+
+        int left=0;
+        int right=1;
+        while (right < arr.length) {
+            if (arr[left] > arr[right]) {
+                left=right;
+                right++ ;
+            }
+            else if(arr[left] < arr[right]){
+                left++;
+                right--;
+            }
+            else{
+                right++;
+            }
+        }
+        return arr;
     }
 }
